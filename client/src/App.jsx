8cc07046038a312412ac29e23fb03d61f9cd73c8
Vipul Sharma <sharmavipul9756@gmail.com>
2022-01-08 23:41:31 +0530

    first

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+const  App = () => {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline">
+          hello world
+        </h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
